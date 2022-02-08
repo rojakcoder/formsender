0bd06180e98c9f71e49ed45bfeb1aa14bfee4041
Chua Chee How <chuacheehow@gmail.com>
2022-02-08 15:03:28 +0800

    Added CORS capability.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors, { CorsOptions } from 'cors';
 import express from 'express';
 import { Bot } from 'grammy';
 import Mustache from 'mustache';
@@ -7,17 +8,30 @@ const PORT = 7027;
 
 const bot = new Bot('2107641114:AAG1NGCnIyC7FYTm3A7aXRlmAzuqjzUmpUM');
 
-app.use(express.json());
+const origins = ['https://www.auracapital.io:7027'];
+const corsOptions = {
+  origin: function (origin: string, callback: Function) {
+    if (origins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  preflightContinue: false,
+};
+
+app.use(cors(corsOptions as CorsOptions), express.json());
 
 app.get('/health', (req, res) => {
   return res.send('Form server is running.');
 });
 
 app.post('/message', (req, res) => {
+  console.info(req.body);
   const message = Mustache.render(letter, req.body);
 
   bot.api.sendMessage('1107012659', message);
-  console.log(message);
   return res.status(200).send();
 });
 
@@ -31,5 +45,5 @@ const letter = `auracapital.io received a form.
 Name: {{ name }}
 Email address: {{ email }}
 Question/Comment:
-{{ comment }}
+{{ comments }}
 `;
