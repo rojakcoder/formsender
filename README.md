4bf8912cf20efa1e8d52eb082aac05a3d3435f72
Chua Chee How <chuacheehow@gmail.com>
2022-05-08 20:57:18 +0800

    Made configurable settings into TOML and .env files.
    - Added the ability to read from a TOML configuration file.
    - Added ability to read from the .env file.

---
+++ README.md
@@ -0,0 +1,13 @@
+This repository sends messages to specified Telegram groups.
+
+It is able to receive requests from sites that are specified in the
+configuration files as allowable origins.
+
+## Running
+
+The service can be run like so:
+
+```bash
+npm start
+```
+
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
 import cors, { CorsOptions } from 'cors';
 import express from 'express';
+import fs from 'fs';
 import { Bot } from 'grammy';
 import Mustache from 'mustache';
+import toml from 'toml';
+
+if (!process.env.telegram_key) {
+  console.error(
+    `Expect environment variable "telegram_key" to contain the private key for the Telegram bot.`
+  );
+  process.exit(1);
+}
+
+const config = _readConfig('./config.toml');
+if (!config || !config.server || !Array.isArray(config.server.origins)) {
+  console.error(`Configuration file does not contain "server.origins" array.`);
+  process.exit(1);
+}
+if (!config || !config.telegram || !Array.isArray(config.telegram.groups)) {
+  console.error(`Configuration file does not contain "telegram.groups" array.`);
+  process.exit(1);
+}
 
 const app = express();
-const PORT = 7027;
+const PORT = config.server?.port ?? 7027;
 
-const bot = new Bot('');
+const bot = new Bot(process.env.telegram_key);
 
-const origins = ['https://www.auracapital.io'];
 const corsOptions = {
   origin: function (origin: string, callback: Function) {
-    if (origins.indexOf(origin) !== -1 || !origin) {
+    if (config.server.origins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('not allowed by CORS'));
@@ -29,9 +47,14 @@ app.get('/health', (req, res) => {
 
 app.post('/message', (req, res) => {
   console.info(req.body);
+  if (!req.body.domain) {
+    req.body.domain = 'Website';
+  }
   const message = Mustache.render(letter, req.body);
 
-  bot.api.sendMessage('1107012659', message);
+  for (const group of config.telegram.groups) {
+    bot.api.sendMessage(group, message);
+  }
   return res.status(200).send('Message sent.');
 });
 
@@ -39,7 +62,7 @@ app.listen(PORT, () => {
   console.info('Server started.');
 });
 
-const letter = `auracapital.io received a form.
+const letter = `{{ domain }} received a form.
 ---
 
 Name: {{ name }}
@@ -47,3 +70,25 @@ Email address: {{ email }}
 Question/Comment:
 {{ comments }}
 `;
+
+function _readConfig(file: string) {
+  let config;
+  let raw;
+  try {
+    raw = fs.readFileSync(file, {
+      encoding: 'utf-8',
+      flag: 'r',
+    });
+  } catch (err: any) {
+    console.error(`Unable to read file "${file}":`, err.toString());
+    process.exit(1);
+  }
+
+  try {
+    config = toml.parse(raw);
+  } catch (err: any) {
+    console.error(`Unable to parse TOML file:`, err.toString());
+    process.exit(1);
+  }
+  return config;
+}
